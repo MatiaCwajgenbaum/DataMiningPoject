f36349ad5c7674bfe40d974de096d1400a78916b
Matia cwajgenbaum <matiac99@gmail.com>
2022-03-03 19:14:50 +0200

    Last commit main.py and TweetsExtr after last modifications for Checkp1

--- Tweets_Extractor.py
+++ Tweets_Extractor.py
@@ -3,13 +3,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import csv
-from selenium.webdriver.chrome.service import Service
+import chromedriver_autoinstaller
+
 
 class Tweets_extractor:
-    def __init__(self, search_term, path_web, quantity_of_tweets, path_file):
+    """
+    Create a Tweets extractor object that can be called after creating a Chrome driver object to extract
+    and store in a table as many tweets as one wants, from the result's page of a specific term research on Twitter
+    """
+
+    def __init__(self, search_term, quantity_of_tweets, path_CSVfile):
+        """
+        Our class has 3 attributes the user need to enter: 1) the terms he wants to type in the search bar.
+          2)How many tweets does he want to extract
+        and 3) the path of the csv file in which the tweets will be saved
+
+        There are other attributes corresponding to the different features the user can extract for each tweet
+         from the page, for example, self.list_hashtags is a list of the hashtags appearing on each tweet:
+         example: self.list_hashtags[i] is the list of hashtags present with the i th tweet.
+         !!! However, these last attributes are empty until the user fill them with the corresponding methods !
+        """
         self.search_term = search_term
-        self.path_web = path_web
-        self.path_file = path_file
+        self.path_CSVfile = path_CSVfile
         self.quantity_of_tweets = quantity_of_tweets
 
         self.list_of_publishers = []
@@ -24,10 +39,12 @@ class Tweets_extractor:
         self.list_videos = []
 
     def _scroll_down_page(self, driver, last_position, num_seconds_to_load=0.5, scroll_attempt=0, max_attempts=5):
-        """The function will try to scroll down the page and will check the current
+        """
+        The function will try to scroll down the page and will check the current
         and last positions as an indicator. If the current and last positions are the same after `max_attempts`
         the assumption is that the end of the scroll region has been reached and the `end_of_scroll_region`
-        flag will be returned as `True`"""
+        flag will be returned as `True`
+        """
         end_of_scroll_region = False
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         sleep(num_seconds_to_load)
@@ -41,7 +58,9 @@ class Tweets_extractor:
         return last_position, end_of_scroll_region
 
     def _save_tweet_data_to_csv(self, records, filepath, mode='w'):
-        """create csv file with all the records"""
+        """
+        create csv file with all the records
+        """
         header = ['NAMES OF PUBLISHERS', 'HASHTAGS', 'USERS TAGGED', 'LINKS', 'ReplyCount', 'RetweetCount', 'LikeCount',
                   'DATES', 'NUMBER OF IMAGES']
         with open(filepath, mode=mode, newline='', encoding='utf-8') as f:
@@ -51,20 +70,23 @@ class Tweets_extractor:
                 writer.writerow(row)
 
     def _create_webdriver_instance(self):
+        """
+        returns a Webdriver object which will navigate through the webpages and extract the data we want
+        """
         options = Options()
         options.headless = True  # hide GUI
         options.add_argument("--window-size=1920,1080")  # set window size to native GUI size
         options.add_argument("start-maximized")
-        s = Service(PATH)
 
+        chromedriver_autoinstaller.install()
 
-        driver = webdriver.Chrome(service=s)
-
-
+        driver = webdriver.Chrome(options=options)
         return driver
 
     def initialize_driver(self):
-        """initialize the wanted page"""
+        """
+        initialize the wanted page by the self._create_webdriver_instance() method to generate the webdriver
+        """
         driver = self._create_webdriver_instance()
         driver.get('https://twitter.com/search?q=' + self.search_term)
         driver.implicitly_wait(10)
@@ -72,12 +94,15 @@ class Tweets_extractor:
         return driver
 
     def Return_attribute(self, attribute):
-        """return attribute"""
+        """
+        return attribute
+        """
         return getattr(self, attribute)
 
-        ####     NAMES OF PUBLISHERS
-
     def _Extract_Names(self, driver):
+        """
+        Returns a list of the names of the different tweet publishers
+        """
         Publishers = driver.find_elements(By.CLASS_NAME,
                                           "css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1wbh5a2 r-dnmrzs r-1ny4l3l".replace(
                                               ' ', '.'))
@@ -88,11 +113,11 @@ class Tweets_extractor:
             else:
                 self.list_of_publishers.append(Publishers[i])
 
-
-        ####       HASHTAGS, USERS TAGGED, LINKS
-
     def _Extract_hashtags_userstagged_links(self, driver):
-
+        """"
+        Returns 3 lists containing for each tweet the different hashtags (self.list_hashtags),
+         the users tagged (self.list_users_tagged),  and the links (self.list_links)
+        """
         text_info = driver.find_elements(By.CLASS_NAME,
                                          "css-901oao r-18jsvk2 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0".replace(
                                              ' ', '.'))
@@ -119,7 +144,9 @@ class Tweets_extractor:
             ####         NUMBERS OF REPLIES, RETWEETS AND LIKES
 
     def _Extract_NumReplies_retweets_likes(self, driver):
-
+        """"
+        Returns a list containing 3 numbers per tweet: Num of replies, retweets and likes
+        """
         number_info = driver.find_elements(By.CLASS_NAME,
                                            "css-1dbjc4n r-1ta3fxp r-18u37iz r-1wtj0ep r-1s2bzr4 r-1mdbhws".replace(' ',
                                                                                                                    '.'))
@@ -135,17 +162,21 @@ class Tweets_extractor:
         ####      DATES
 
     def _Extract_Dates(self, driver):
-
+        """
+        Returns a list of the exact dates the tweets extracted where posted at.
+        """
         TIMES = driver.find_elements(By.TAG_NAME, 'time')
         for item in TIMES:
             time = item.get_attribute('datetime')
             time = time.replace('T', ' ')
             self.list_times.append(time.replace('Z', ' '))
 
-        ####           NUMBER OF IMAGES
+
 
     def _Extract_Num_images(self, driver):
-
+        """
+        Returns a list giving the number of images present in every tweet.
+        """
         list_tweet = driver.find_elements(By.CLASS_NAME,
                                           "css-1dbjc4n r-1ets6dv r-1867qdf r-1phboty r-rs99b7 r-1ny4l3l r-1udh08x r-o7ynqc r-6416eg".replace(
                                               ' ', '.'))
@@ -154,12 +185,11 @@ class Tweets_extractor:
             images_for_tweet = i.find_elements(By.TAG_NAME, "img")
             self.list_images.append(len(images_for_tweet))
 
-
-
     def Extract_ALL(self, driver):
 
         """
-        Returns a csv file which contains all the features extracted from the webpage
+        Creates and Stores a csv file which contains all the features extracted from the webpage, the file is located in
+        the path given by the user when creating the TweetsExtractor object
         """
 
         last_position = None
@@ -173,9 +203,14 @@ class Tweets_extractor:
             last_position, end_of_scroll_region = self._scroll_down_page(driver, last_position)
             if int(self.quantity_of_tweets) < len(self.list_of_publishers):
                 break
-        records = zip(self.list_of_publishers, self.list_hashtags, self.list_users_tagged, self.list_links,
-                      self.list_number_reply, self.list_number_retweet, self.list_number_Like, self.list_times,
-                      self.list_images)
+        records = zip(self.list_of_publishers[:int(self.quantity_of_tweets)],
+                      self.list_hashtags[:int(self.quantity_of_tweets)],
+                      self.list_users_tagged[:int(self.quantity_of_tweets)],
+                      self.list_links[:int(self.quantity_of_tweets)],
+                      self.list_number_reply[:int(self.quantity_of_tweets)],
+                      self.list_number_retweet[:int(self.quantity_of_tweets)],
+                      self.list_number_Like[:int(self.quantity_of_tweets)],
+                      self.list_times[:int(self.quantity_of_tweets)],
+                      self.list_images[:int(self.quantity_of_tweets)])
 
-        self._save_tweet_data_to_csv(records, self.path_file)
-
+        self._save_tweet_data_to_csv(records, self.path_CSVfile)
--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import sys
 from Tweets_Extractor import *
 
 
-def main(search_term, path_web, quantity_of_tweets, path_file):
-    tweets = Tweets_extractor(search_term, path_web, quantity_of_tweets, path_file)
-    driver = tweets.initialize_driver()
-    tweets.Extract_ALL(driver)
-    driver.quit()
+def main(search_term, quantity_of_tweets, path_CSVfile):
+    """
+    arguments: the term the user wants to type in the search bar, the num of tweets the user wants to extract
+     and the path where the csv file will be created.
+     Returns the csv file containing the extracted tweets and located in the path as required.
+    """
+    try:
+        tweets = Tweets_extractor(search_term, quantity_of_tweets, path_CSVfile)
+        driver = tweets.initialize_driver()
+        tweets.Extract_ALL(driver)
+        driver.quit()
+    except ValueError as va:
+        print(f'{va}, Make sure you type an integer as second argument !')
+    except PermissionError as Pa:
+        print(f'{Pa}, Make sure the path tou typed really exists or that you have permission to access it! !')
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        raise Exception('Sorry, you need to type at least two arguments ! 1) the text you want to search in twitter and\
+    2) the number of tweets you need')
     term = sys.argv[1]  # 'GoodNews'  # #'GoodNews'#'pysimplegui'
     number_of_tweets = sys.argv[2]
-    pathweb = sys.argv[3]
-    if len(sys.argv) == 5:
-        pathfile = sys.argv[4]
+    if len(sys.argv) >= 4:
+        pathfile = sys.argv[3]
     else:
         pathfile = term + '.csv'
-
-    main(term, pathweb, number_of_tweets, pathfile)
+    main(term, number_of_tweets, pathfile)
