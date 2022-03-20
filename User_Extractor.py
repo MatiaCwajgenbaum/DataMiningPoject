c3566751330906caa39627ecc4a54f0219048020
Maya Yanko <mayayank95@gmail.com>
2022-03-20 14:21:39 +0200

    create and update sql database

--- User_Extractor.py
+++ User_Extractor.py
@@ -8,25 +8,31 @@ import chromedriver_autoinstaller
 
 class User_extractor:
 
-    def __init__(self, list_of_publishers_links, driver):
+    def __init__(self, list_of_publishers_links):
         """
         """
         self.list_of_publishers_links = list_of_publishers_links
         self.list_of_Publishers = []
         self.list_of_Following = []
         self.list_of_Followers = []
-        self.user_extractor(driver)
 
-    def user_extractor(self, driver):
+    def user_extract(self, driver):
         for user_link in self.list_of_publishers_links:
             driver.get(user_link)
             driver.implicitly_wait(10)
             user_info = driver.find_elements(By.CLASS_NAME,
                                              "css-901oao css-16my406 r-18jsvk2 r-poiln3 r-b88u0q r-bcqeeo r-qvutc0".replace(
                                                  ' ', '.'))
+            user_name = driver.find_elements(By.CLASS_NAME,
+                                             "css-901oao css-bfa6kz r-18u37iz r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0".replace(
+                                                 ' ', '.'))
             try:
-                self.list_of_Publishers.append(user_info[0].text)
+                self.list_of_Publishers.append(user_name[0].text)
                 self.list_of_Following.append(user_info[1].text)
                 self.list_of_Followers.append(user_info[2].text)
             except IndexError:
                 print(f"This link, {user_link} is not accessible")
+
+
+        records = zip(self.list_of_Publishers, self.list_of_Following, self.list_of_Followers)
+        return records
--- build_database.py
+++ build_database.py
@@ -12,6 +12,7 @@ def execute_sql(sql):
     cursor.execute(sql)
     result = cursor.fetchall()
     print(result)
+    return result
 
 
 def create_DATABASE():
@@ -23,19 +24,28 @@ def create_DATABASE():
     # sql = "SHOW databases"
     # execute_sql(sql)
 
+
 def use_DATABASE():
     sql = "USE Tweets"
     execute_sql(sql)
 
-def create_table(search_term):
-    sql = f"""CREATE TABLE Tweet_{search_term}(
+
+def get_number_of_rows(table):
+    sql = f'select count(*) from {table}'
+    result = execute_sql(sql)
+    return result[0]['count(*)']
+
+
+def create_table_tweets():
+    sql = f"""CREATE TABLE Tweets(
              tweet_id INT PRIMARY KEY,
-             NAMES_OF_PUBLISHERS VARCHAR(30),
-             ReplyCount INT,
-             RetweetCount INT,
-             LikeCount INT,
-             DATES DATE,
-             NUMBER_OF_IMAGES INT
+             NAME_OF_PUBLISHER VARCHAR(30),
+             ReplyCount VARCHAR(10),
+             RetweetCount VARCHAR(10),
+             LikeCount VARCHAR(10),
+             DATES VARCHAR(30),
+             NUMBER_OF_IMAGES INT,
+             search_term VARCHAR(30)
     )"""
     try:
         execute_sql(sql)
@@ -45,7 +55,79 @@ def create_table(search_term):
     # execute_sql(sql)
 
 
-def update_table(records, search_term):
-    for index, row in enumerate(records):
-        sql = f"INSERT INTO Tweet_{search_term} VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        cursor.execute(sql, (index, row[0], row[4], row[5], row[6], row[7], row[8]))
+def update_table_tweets(records, search_term, number_of_rows):
+    index = 0
+    for row in records:
+        sql = f'select count(*) from Tweets where DATES = "{row[7]}" AND NAME_OF_PUBLISHER= {row[0]}'
+        result = execute_sql(sql)
+        if result[0]['count(*)'] != 0:
+            continue
+        ezer_row4 = row[4]
+        if ezer_row4 == '':
+            ezer_row4 = 0
+        ezer_row5 = row[5]
+        if ezer_row5 == '':
+            ezer_row5 = 0
+        ezer_row6 = row[6]
+        if ezer_row6 == '':
+            ezer_row6 = 0
+        sql = "INSERT INTO Tweets VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql,
+                       ((number_of_rows + index), row[0], ezer_row4, ezer_row5, ezer_row6, row[7], row[8], search_term))
+        index = index + 1
+    connection.commit()
+
+
+def create_table_users():
+    sql = f"""CREATE TABLE Users(
+             user_id INT PRIMARY KEY,
+             NAME_OF_PUBLISHER VARCHAR(30),
+             Number_of_Following VARCHAR(10),
+             Number_of_Followers VARCHAR(10)
+    )"""
+    try:
+        execute_sql(sql)
+    except pymysql.err.OperationalError as err:
+        print(err)
+
+
+def update_table_users(records, number_of_rows):
+    index = 0
+    for row in records:
+        sql = f'select count(*) from Users where NAME_OF_PUBLISHER = "{row[0]}"'
+        result = execute_sql(sql)
+        if result[0]['count(*)'] != 0:
+            continue
+        sql = "INSERT INTO Users VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql,
+                       (number_of_rows + index, row[0], row[1], row[2]))
+        index = index + 1
+    connection.commit()
+
+# def create_table_hashtag():
+#     sql = f"""CREATE TABLE Hashtag(
+#              id INT PRIMARY KEY,
+#              tweet_id INT
+#     )"""
+#     try:
+#         execute_sql(sql)
+#     except pymysql.err.OperationalError as err:
+#         print(err)
+
+# def create_table_hashtag():
+#     sql = f"""CREATE TABLE hashtag(
+#              id INT PRIMARY KEY,
+#              NAME VARCHAR(30)
+#     )"""
+#     try:
+#         execute_sql(sql)
+#     except pymysql.err.OperationalError as err:
+#         print(err)
+#
+#
+# def update_table_hashtag(records, number_of_rows):
+#     for index, row in enumerate(records):
+#         sql = "INSERT INTO Tweets VALUES (%s, %s)"
+#         cursor.execute(sql,
+#                        (number_of_rows + index, row[0]))
+#     connection.commit()
