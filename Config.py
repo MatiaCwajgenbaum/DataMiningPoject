bc8fdb967fe6ed87917a7a33e2787f4095c3a0e7
Maya Yanko <mayayank95@gmail.com>
2022-04-26 18:03:23 +0300

    update API

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
 # User has to type here his own id and password for mysql
-USER = "root"
-PASSWORD = "PASSWORD..."
+USER = "maya95"
+PASSWORD = "MSmaya2035!"
 HOST = "localhost"
 
 LENGTH_VARCHAR = 255
@@ -9,7 +9,7 @@ NUM_SECONDS_TO_LOAD = 0.5
 MAX_NUM_TWEETS = 100
 
 # This variable is the location of the text file where the user stored his twitter developper keys and tokens
-TOKEN_LOCATION = r"C:\Users\matia\OneDrive\Bureau\Twitter_api_mafteah.txt"
+TOKEN_LOCATION = r"/home/maya/PycharmProjects/itc/Twitter_api_mafteah.txt"#r"C:\Users\matia\OneDrive\Bureau\Twitter_api_mafteah.txt"
 USER_FIELDS = "user.fields=description,created_at,public_metrics,verified"
 
 # log file
